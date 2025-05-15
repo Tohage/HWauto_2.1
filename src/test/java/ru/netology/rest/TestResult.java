d7fc4fd7522355939e84469d087127fe1a086ba6
Tohage89 <avgorin89@gmail.com>
2025-05-15 18:41:22 +0300

    test passing

--- src/test/java/ru/netology/rest/TestResult.java
+++ src/test/java/ru/netology/rest/TestResult.java
@@ -19,7 +19,7 @@ public class TestResult {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("fale"))
+                .body("data", equalTo("HWauto"))
         ;
     }
 
